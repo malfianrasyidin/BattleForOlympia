24a9f39f61647b3324f56ea50d30d644e50d9564
Mochamad Alghifari <13516038@std.stei.itb.ac.id>
2017-11-14 09:06:10 +0700

    nambahin printMap sama MatGen(NB,NK)*generate map awal

--- MatriksMap.c
+++ MatriksMap.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "boolean.h"
 #include "unit.h"
+#include "point.h"
 
 //FUNGSI NULLITAS
 Build NullBuilding(){
@@ -11,6 +12,8 @@ Build NullBuilding(){
 	B.Tipe = " ";
 	return (B);
 }
+
+
 
 /* ********** DEFINISI PROTOTIPE PRIMITIF ********** */              
 
@@ -69,75 +72,84 @@ boolean IsUnitIn (POINT P, MatriksMap M){
 
 Unit getUnit(POINT P, MatriksMap M){
 //Mengembalikan unit yang berada di titik
-	return (Elmt(M,Absis(P),Ordinat(P)).Res);
+	return UnitIn(Elmt(M,Absis(P),Ordinat(P)));
 }
 
-// void PrintMap(MatriksMap M){
-// /*Memprint Peta M ke Layar*/
-// 	for (int i = 1 ; i <= NBrsEff(M) ; i++){
-// 		printf("#");
-// 		for (int j = 1 ; j <= NKolEff(M) ; j++){
-// 			printf("______");
-// 		}
-// 		printf("#\n#");
-// 		for (int j = 1 ; j <= NKolEff(M) ; j++){
-// 			printf("| %c |", Building(Elmt(M,i,j)));
-// 		}
-// 		printf("#\n#");
-// 		for (int j = 1 ; j <= NKolEff(M) ; j++){
-// 			printf("| %c |", UnitIn(Elmt(M,i,j)));
-// 		}
-// 		printf("#\n#");
-// 		for (int j = 1 ; j <= NKolEff(M) ; j++){
-// 			printf("|    |");
-// 		}
-// 		printf("#\n#");
-// 		for (int j = 1 ; j <= NKolEff(M) ; j++){
-// 			printf("______");
-// 		}
-// 		printf("#\n");
-// 	}
-// }
+Build getBuild(POINT P, MatriksMap M)
+//Mengembalikan build yang berada di titik P.
+{
+	return BuildIn(Elmt(M,Absis(P),Ordinat(P)));	
+}
 
-// MatriksMap MatGen(indeks NB, indeks NK){
-// /*MengGenerate Peta yang baru dengan Random Number */
-// 	int R;
-// 	MatriksMap M;
-// 	MakeMATRIKS(NB,NK, &M);
-// 	int SB = NB/2;
-// 	Building(Elmt(M,SB,2)) = 'T';
-// 	Owner(Elmt(M,SB,2)) = 1;
-// 	Building(Elmt(M,SB,1)) = 'C';
-// 	Owner(Elmt(M,SB,2)) = 1;
-// 	Building(Elmt(M,SB,3)) = 'C';
-// 	Owner(Elmt(M,SB,3)) = 1;
-// 	Building(Elmt(M,SB + 1, 2)) = 'C';
-// 	Owner(Elmt(M,SB + 1,2)) = 1;
-// 	Building(Elmt(M,SB - 1, 2)) = 'C';
-// 	Owner(Elmt(M,SB - 1, 2)) = 1;
-// 	Building(Elmt(M,SB,NK - 1)) = 'T';
-// 	Owner(Elmt(M,SB,NK - 1)) = 2;
-// 	Building(Elmt(M,SB,NK)) = 'C';
-// 	Owner(Elmt(M,SB,NK)) = 2;
-// 	Building(Elmt(M,SB,NK + 1)) = 'C';
-// 	Owner(Elmt(M,SB,NK + 1)) = 2;
-// 	Building(Elmt(M,SB + 1, NK)) = 'C';
-// 	Owner(Elmt(M,SB + 1, NK)) = 2;
-// 	Building(Elmt(M,SB - 1, NK)) = 'C';
-// 	Owner(Elmt(M,SB - 1, NK)) = 2;
-// 	for (int i = 1 ; i <= NBrsEff(M) ; i++){
-// 		for (int j = 1 ; j <= NKolEff(M) ; j++){
-// 			if (Building(Elmt(M,i,j)) != ' '){
-// 				R = (unsigned) rand() % 4;
-// 				if(R == 0){
-// 					Building(Elmt(M,i,j)) = 'V';
-// 				}
-// 				else {
-// 					Building(Elmt(M,i,j)) = ' ';
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return(M);
-// }
+void PrintMap(MatriksMap M){
+/*Memprint Peta M ke Layar*/
+	for (int i = 1 ; i <= NBrsEff(M) ; i++){
+		printf("#");
+		for (int j = 1 ; j <= NKolEff(M) ; j++){
+			printf("______");
+		}
+		printf("#\n#");
+		for (int j = 1 ; j <= NKolEff(M) ; j++){
+			printf("| %s |", &(BuildIn(Elmt(M,i,j))).Tipe[0]);
+		}
+		printf("#\n#");
+		for (int j = 1 ; j <= NKolEff(M) ; j++){
+			printf("| %s |", &(UnitIn(Elmt(M,i,j))).KarType[0]);
+		}
+		printf("#\n#");
+		for (int j = 1 ; j <= NKolEff(M) ; j++){
+			printf("|    |");
+		}
+		printf("#\n#");
+		for (int j = 1 ; j <= NKolEff(M) ; j++){
+			printf("______");
+		}
+		printf("#\n");
+	}
+}
+
+MatriksMap MatGen(indeks NB, indeks NK){
+/*MengGenerate Peta yang baru dengan Random Number */
+	int R;
+	MatriksMap M;
+	MakeMATRIKS(NB,NK, &M);
+	//player 1
+	BuildIn(Elmt(M,NB-1,KolMin+1)).Tipe = "Tower";
+	BuildIn(Elmt(M,NB-1,KolMin+1)).Owner = 1;
+	POINT K1 = MakePOINT(NB-1,KolMin+1);
+	UnitIn(Elmt(M,NB-1,KolMin+1)) = InitKing(K1,1);
+	BuildIn(Elmt(M,NB-2,KolMin+1)).Tipe = "Castle"; //atas
+	BuildIn(Elmt(M,NB-2,KolMin+1)).Owner = 1;
+	BuildIn(Elmt(M,NB-1,KolMin)).Tipe = "Castle"; //kiri
+	BuildIn(Elmt(M,NB-1,KolMin)).Owner = 1;
+	BuildIn(Elmt(M,NB-1,KolMin+2)).Tipe = "Castle"; //kanan
+	BuildIn(Elmt(M,NB-1,KolMin+2)).Owner = 1;
+	BuildIn(Elmt(M,NB,KolMin+1)).Tipe = "Castle"; //bawah
+	BuildIn(Elmt(M,NB,KolMin+1)).Owner = 1;
+	//player 2
+	BuildIn(Elmt(M,BrsMin+1,NK-1)).Tipe = "Tower";
+	BuildIn(Elmt(M,BrsMin+1,NK-1)).Owner = 2;
+	POINT K2 = MakePOINT(BrsMin+1,NK-1);
+	UnitIn(Elmt(M,BrsMin+1,NK-1)) = InitKing(K2,2);
+	BuildIn(Elmt(M,BrsMin,NK-1)).Tipe = "Castle"; //atas
+	BuildIn(Elmt(M,BrsMin,NK-1)).Owner = 2;
+	BuildIn(Elmt(M,BrsMin+1,NK-2)).Tipe = "Castle"; //kiri
+	BuildIn(Elmt(M,BrsMin+1,NK-2)).Owner = 2;
+	BuildIn(Elmt(M,BrsMin+1,NK)).Tipe = "Castle"; //kanan
+	BuildIn(Elmt(M,BrsMin+1,NK-2)).Owner = 2;
+	BuildIn(Elmt(M,BrsMin+2,NK-1)).Tipe = "Castle"; //bawah
+	BuildIn(Elmt(M,BrsMin+2,NK-1)).Owner = 2;
+	
+	int i,j,k;
+	for (k=1; k<=3; k++)	{
+		i = rand() % NB;
+		j = rand() % NK;
+		while (BuildIn(Elmt(M,i,j)).Tipe!=" ")	{
+			i = rand() % NB;
+			j = rand() % NK;
+		}
+		BuildIn(Elmt(M,i,j)).Tipe="Village";
+	}
+	return(M);
+}
 
