84b7bde117fe5f90a2cc712d220edf2de24dc72e
Mochamad Alghifari <13516038@std.stei.itb.ac.id>
2017-11-14 11:16:49 +0700

    PrintInfoUnit & CmpUnit*compare & init unit & bersih2 code

--- unit.c
+++ unit.c
@@ -2,6 +2,9 @@
 #include "point.h"
 #include "boolean.h"
 #include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 //FUNGSI NULLITAS
 Unit NullUnit(){
@@ -22,6 +25,17 @@ Unit InitArcher(POINT P,int Owner)
 //F.S : King dengan state awal
 {
 	Unit U;
+	Locate(U)=P;
+	MaxHP(U)=BaseMaxHPArcher;
+	HP(U)=BaseMaxHPArcher;
+	MaxMP(U)=BaseMaxMPArcher;
+	MP(U)=BaseMaxMPArcher;
+	Owner(U)=Owner;
+	Tipe(U)="Archer";
+	CanAtk(U)=true;
+	Attack(U,1).Dmg=45;
+	Attack(U,1).Type="Ranged";
+	Price(U)=5;
 	return U;
 }
 
@@ -29,6 +43,17 @@ Unit InitWarrior(POINT P,int Owner)
 //F.S : King dengan state awal
 {
 	Unit U;
+	Locate(U)=P;
+	MaxHP(U)=BaseMaxHPWarrior;
+	HP(U)=BaseMaxHPWarrior;
+	MaxMP(U)=BaseMaxMPWarrior;
+	MP(U)=BaseMaxMPWarrior;
+	Owner(U)=Owner;
+	Tipe(U)="Warrior";
+	CanAtk(U)=true;
+	Attack(U,1).Dmg=40;
+	Attack(U,1).Type="Melee";
+	Price(U)=4;
 	return U;
 }
 
@@ -36,6 +61,17 @@ Unit InitMage(POINT P,int Owner)
 //F.S : King dengan state awal
 {
 	Unit U;
+	Locate(U)=P;
+	MaxHP(U)=BaseMaxHPMage;
+	HP(U)=BaseMaxHPMage;
+	MaxMP(U)=BaseMaxMPMage;
+	MP(U)=BaseMaxMPMage;
+	Owner(U)=Owner;
+	Tipe(U)="Mage";
+	CanAtk(U)=true;
+	Attack(U,1).Dmg=30;
+	Attack(U,1).Type="Melee";
+	Price(U)=5;
 	return U;
 }
 
@@ -50,7 +86,7 @@ Unit InitKing(POINT P,int Owner)
 	MP(U)=BaseMaxMPKing;
 	Owner(U)=Owner;
 	Tipe(U)="King";
-	Attack(U,1).Name="Sword";
+	CanAtk(U)=true;
 	Attack(U,1).Dmg=50;
 	Attack(U,1).Type="Melee";
 	return U;
@@ -73,60 +109,35 @@ int Distance(POINT P1, POINT P2){
 }
 
 boolean IsAdjacent (POINT P1, POINT P2){
-//Mengeluarkan True Jika P1 dan P2 Adjacent atau beda sepetak dengan U2.
+//Mengeluarkan true Jika P1 dan P2 Adjacent atau beda sepetak dengan U2.
 	return (Distance(P1,P2)==1);
 }
 
-// boolean IsTopPresent(Unit U){
-// //Mengembalikan True jika terdapat unit lain di sebelah atas unit U.
-// 	Location L;
-// 	L.Loc = PlusDelta(Locate(U).Loc, 0, 1);
-// 	return (IsUnitIn(L));
-// }
-
-// boolean IsBottomPresent(Unit U){
-// //Mengembalikan True jika terdapat unit lain di sebelah bawah unit U.
-// 	Location L;
-// 	L.Loc = PlusDelta(Locate(U).Loc, 0, -1);
-// 	return (IsUnitIn(L));
-// }
-
-// boolean IsRightPresent(Unit U){
-// //Mengembalikan True jika terdapat unit lain di sebelah kanan unit U.
-// 	Location L;
-// 	L.Loc = PlusDelta(Locate(U).Loc, 1, 0);
-// 	return (IsUnitIn(L));
-// }
-
-// boolean IsLeftPresent(Unit U){
-// //Mengembalikan True jika terdapat unit lain di sebelah kiri unit U.
-// 	Location L;
-// 	L.Loc = PlusDelta(Locate(U).Loc, -1, 0);
-// 	return (IsUnitIn(L));
-// }
-
 boolean IsEnemy(Unit U1, Unit U2){
-//Mengembalikan True jika U2 adalah musuh dari U1 (U1 ialah player yang sedang bermain)
+//I.S:U1 && U2 tidak null
+//Mengembalikan true jika U2 adalah musuh dari U1 (U1 ialah player yang sedang bermain)
 	return(Owner(U1)!=Owner(U2));
 }
 
-// boolean IsCanAttack(Unit U){
-// //Mengembalikan True jika U dapat melakukan attack.	
-// 	boolean Top, Bottom, Right, Left;
-// 	POINT PTop,PBottom, PRight, PLeft;
-
-// 	PTop = NextY(Locate(U));
-// 	PRight = NextX(Locate(U));
-// 	PLeft = PlusDelta(Locate(U).Loc, -1, 0);
-// 	PBottom = PlusDelta(Locate(U).Loc, 0, -1);
-
-// }
-
-Unit MakeUnit (int Owner, char Type, POINT Loc);
-//Membuat Unit Baru dengan Owner, Type, dan Location yang dispesifikkan
+void PrintInfoUnit (Unit U)
+/* mengeluarkan info unit standar seperti dibawah ini
+Unit: King(2,1) | Health 20/20 | Movement Point: 2 | Can Attack: Yes */
+{
+	printf("Unit: %s (%d,%d) | ", Tipe(U), Absis(Locate(U)), Ordinat(Locate(U)));
+	printf("Health: %d/%d | ", HP(U), MaxHP(U));
+	printf("Movement Point: %d | ", MP(U));
+	if (CanAtk(U))	printf("Can Attack: Yes\n");
+	else	printf("Can Attack: No\n");
+}
 
 void AttackU (Unit U1, Unit U2, Atk A);
 //Membuat Unit 1 Menyerang Unit 2 dengan tipe Attack 1
 
 void PrintAtkType(Unit U);
 //Memprint ke layar Tipe Attack yang dapat dilakukan oleh Unit U
+
+boolean CmpUnit (Unit U1, Unit U2)
+//True jika U1==U2
+{
+	return(EQ(Locate(U1),Locate(U2)));
+}
